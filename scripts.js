62e742c5f2fb89d81017701653a549b1ce5b51ea
Adrian Dela Cruz <56340846+adriandc9@users.noreply.github.com>
2024-01-10 18:06:54 +0800

    Upload initial files for this repository

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function toggleEGrid() {
+    var element = document.getElementById("offcanvass-e");
+    element.classList.toggle("left-opened");
+}
+
+function toggleEFlex() {
+    var element = document.getElementById("offcanvass---e");
+    element.classList.toggle("left-opened");
+}
